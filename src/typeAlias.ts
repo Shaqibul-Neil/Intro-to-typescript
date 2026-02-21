9382f5b044b9a90146dd9b5ce0d9d4b16c4f97cd
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-02-22 02:58:48 +0600

    Type Alias in typescript

---
+++ src/typeAlias.ts
@@ -0,0 +1,54 @@
+type User = {
+  id: number;
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  gender: "male" | "female";
+  contact: string;
+  address: {
+    division: string;
+    district: string;
+    thana: string;
+  };
+};
+
+const user1: User = {
+  id: 123,
+  name: {
+    firstName: "Neil",
+    lastName: "Juneja",
+  },
+  gender: "male",
+  contact: "01784545454",
+  address: {
+    division: "Dhaka",
+    district: "Dhaka",
+    thana: "Sher-e-Bangla Nagar",
+  },
+};
+
+const user2: User = {
+  id: 123,
+  name: {
+    firstName: "Shaqibul",
+    lastName: "Islam",
+  },
+  gender: "male",
+  contact: "017844532535",
+  address: {
+    division: "Dhaka",
+    district: "Dhaka",
+    thana: "Sher-e-Bangla Nagar",
+  },
+};
+
+type IsAdmin = true;
+const isAmin: isAdmin = true;
+
+type Name = string;
+const myName: Name = "123";
+
+//type alias in function
+type AddFunc = (num1: number, num2: number) => number;
+const add: AddFunc = (num1, num2) => num1 + num2;

0a06fc093d98e564b88d6ecf5ac003ea00aad699
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-03-11 12:18:15 +0600

    ts revise data types

---
+++ src/conceptual.ts
@@ -0,0 +1,35 @@
+//tuple--> pattern match
+//let user: [string, number] = ["Shaqib", 34];
+
+//****************************************** */
+
+//enum-->enumeration-->fixed value constant, multiple value
+//roles--> customer, seller, admin, superAdmin
+
+// enum TrafficLight {
+//   RED,
+//   YELLOW,
+//   GREEN,
+// }
+// enum ROLE {
+//   ADMIN,
+//   SUPERADMIN,
+// }
+// if (ROLE.ADMIN === "ADMIN") {
+// }
+// let signal: TrafficLight = TrafficLight.RED;
+
+// console.log(typeof TrafficLight);
+// console.log(signal);
+
+//****************************************** */
+// function greet(): void {
+//   console.log("good morning");
+// }
+// //greet();
+
+// function add(a: number, b: number): number {
+//   return a + b;
+// }
+
+//****************************************** */

a4a63d8a033dde2b18bdf4bc2d4ccaecdb28f3ec
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-05-01 18:07:08 +0600

    void, never, unknown

--- dist/nullableUnknownNever.js
+++ dist/nullableUnknownNever.js
@@ -15,16 +15,18 @@ const discountCalc = (input) => {
         console.log(input * 0.1);
     }
     else if (typeof input === "string") {
-        console.log(parseInt(input) * 0.2);
+        console.log("str", parseInt(input) * 0.2);
     }
     else
         console.log(input);
 };
-// discountCalc(100);
-// discountCalc("100 TK");
-// discountCalc(null);
+discountCalc(100);
+discountCalc("100 TK");
+discountCalc(null);
 //void
 const throwError = (msg) => {
+    console.log(msg);
     throw new Error(msg);
 };
+//throwError("error");
 //# sourceMappingURL=nullableUnknownNever.js.map
--- src/nullableUnknownNever.ts
+++ src/nullableUnknownNever.ts
@@ -11,15 +11,17 @@ const discountCalc = (input: unknown) => {
   if (typeof input === "number") {
     console.log(input * 0.1);
   } else if (typeof input === "string") {
-    console.log(parseInt(input) * 0.2);
+    console.log("str", parseInt(input) * 0.2);
   } else console.log(input);
 };
 
-// discountCalc(100);
-// discountCalc("100 TK");
-// discountCalc(null);
+discountCalc(100);
+discountCalc("100 TK");
+discountCalc(null);
 
 //void
-const throwError = (msg: string) => {
+const throwError = (msg: string): never => {
+  console.log(msg);
   throw new Error(msg);
 };
+//throwError("error");

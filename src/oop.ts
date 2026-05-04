51b906fb96454cf1eb53f144fc5809bd87624c45
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-05-04 11:24:16 +0600

    Class and object

---
+++ src/oop.ts
@@ -0,0 +1,40 @@
+//Class + Object
+// class Animal {
+//   name: string;
+//   species: string;
+//   sound: string;
+
+//   constructor(name: string, species: string, sound: string) {
+//     this.name = name;
+//     this.species = species;
+//     this.sound = sound;
+//   }
+
+//   //method
+//   makeSound() {
+//     console.log(`${this.name}, the ${this.species} says ${this.sound}`);
+//   }
+// }
+
+//Parameter Property
+class Animal {
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string,
+  ) {}
+
+  //method
+  makeSound() {
+    console.log(`${this.name}, the ${this.species} says ${this.sound}`);
+  }
+}
+
+const Dog = new Animal("Tom", "dog", "Ghew ghew");
+
+const Cat = new Animal("mimi", "cat", "meow meow ");
+
+console.log(Dog.name);
+Dog.makeSound();
+console.log(Dog.species);
+console.log(Dog.sound);

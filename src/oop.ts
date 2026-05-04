9b9b978e3be0617d41ea68817e64ec9b2f4abd41
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-05-04 11:41:48 +0600

    Inheritance, the 1st pilar of OOP

--- src/oop.ts
+++ src/oop.ts
@@ -34,7 +34,42 @@ const Dog = new Animal("Tom", "dog", "Ghew ghew");
 
 const Cat = new Animal("mimi", "cat", "meow meow ");
 
-console.log(Dog.name);
-Dog.makeSound();
-console.log(Dog.species);
-console.log(Dog.sound);
+// console.log(Dog.name);
+// Dog.makeSound();
+// console.log(Dog.species);
+// console.log(Dog.sound);
+
+//Inheritance, the 1st pilar of OOP
+class Parent {
+  constructor(
+    public name: string,
+    public age: number,
+    public address: string,
+  ) {}
+
+  getSleep(numOfHours: number) {
+    console.log(`${this.name} is sleeping for ${numOfHours} hours`);
+  }
+}
+//now new property and method
+class Student extends Parent {}
+
+const student1 = new Student("Shaqib", 34, "Dhaka");
+student1.getSleep(8);
+
+//if new property and method
+class Teacher extends Parent {
+  designation: string;
+
+  constructor(name: string, age: number, address: string, designation: string) {
+    super(name, age, address);
+    this.designation = designation;
+  }
+
+  getTeach(numOfHours: number) {
+    console.log(`${this.name} is taking class for ${numOfHours} hours`);
+  }
+}
+const teacher1 = new Teacher("Neil", 34, "Dhaka", "Teacher");
+
+teacher1.getTeach(10);
